8cec0ce4573b658faa2c66043a36c8d3b44f17e1
Shubham Kumar <shubhamkumar.skv88@gmail.com>
2023-09-12 15:05:59 +0530

    Returns view name instead of ModelAndView.

--- spring-mvc/springmvc/src/main/java/com/shubham/spring/springmvc/controllers/WelcomeController.java
+++ spring-mvc/springmvc/src/main/java/com/shubham/spring/springmvc/controllers/WelcomeController.java
@@ -2,15 +2,12 @@ package com.shubham.spring.springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 
 	@RequestMapping("/welcome")
-	public ModelAndView welcome() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("welcome");
-		return modelAndView;
+	public String welcome() {
+		return "welcome";
 	}
 }
