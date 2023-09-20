86e119ba6fbdc4b7c772bae67b2c85b09d6b8b9e
Shubham Kumar <65139133+shubhamk0808@users.noreply.github.com>
2023-09-20 17:24:20 +0530

    Delete techcode directory

--- techcode/techcode/src/main/java/com/shubham/springmvc/techcode/service/ContactService.java
+++
@@ -1,8 +0,0 @@
-package com.shubham.springmvc.techcode.service;
-
-import com.shubham.springmvc.techcode.model.Contact;
-
-public interface ContactService {
-
-	public boolean saveQueryDetails(Contact contact);
-}
